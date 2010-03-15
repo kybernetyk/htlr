e19e8a187a09db4e69dbb0156b2ab9623a8da9e4
jsz <jszpilewski@me.com>
2010-03-15 21:38:59 +0100

    dontmesswithme

--- app/controllers/spy_controller.rb
+++ app/controllers/spy_controller.rb
@@ -6,19 +6,37 @@ class SpyController < ApplicationController
   def new
     all_items = Item.find(:all)
     
+    if (all_items == nil || !all_items)
+      render :text => "NO ITAMZ"
+      return
+    end
+      
+    
     output = "<table border=1 width=95%>"
     
     for itm in all_items
-      output = output + "<tr><td>"
-      output = output + itm.path_to_file
-      output = output + "</td><td>"
-      output = output + itm.content_type
-      output = output + "</td><td>"
 
-      output = output + "<a href='/" + itm.hosting_hash + "' target=_blank>";
-      output = output + itm.hosting_hash;
-      output = output + "</a>";
-      output = output + "</td></tr>"
+      output = output + "<tr><td>"
+      if (itm.path_to_file)
+        output = output + itm.path_to_file
+      end
+      output = output + "</td>"
+
+      output = output + "<td>"
+      if (itm.content_type)
+        output = output + itm.content_type
+      end
+      output = output + "</td>"
+
+      output = output + "<td>"
+      if (itm.hosting_hash)
+        output = output + "<a href='/" + itm.hosting_hash + "' target=_blank>";
+        output = output + itm.hosting_hash;
+        output = output + "</a>";
+      end
+      output = output + "</td>";
+      
+      output = output + "</tr>"
       
     end
     
